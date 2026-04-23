15601dc503d48c000be53f59a67f1c3c33f1cd9a
Zviviana <137462044+Zviviana@users.noreply.github.com>
2026-04-23 03:18:22 +0000

    Actualizar configuración de ALLOWED_HOSTS y ajustar la construcción de URLs en la API para entornos de Codespace

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,12 @@ SECRET_KEY = 'django-insecure-kjpr$*-i_3!%j#d++uabj**3^&krs1+nj*%)8ug8=xed-0l!u-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f'{CODESPACE_NAME}-8000.app.github.dev')
+    ALLOWED_HOSTS.append(CODESPACE_NAME)
 
 
 # Application definition
